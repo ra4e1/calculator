d3454d2ade6e5b32e3b21e739b0f0258b26eeb62
Rachel <lysova_vasylisa@gmail.com>
2024-02-11 14:01:24 +0300

    Add readme file

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"calculator/application"
 )
 
-func main() { //запуск всего
+func main() { // запуск всего
 	config := application.Config{Port: 8081, CalcDelay: 0}
 	app := application.NewApplication(config)
 	app.Run()
