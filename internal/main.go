18a59d4352a2dc927e675131cc6d524feb666694
Rachel <lysova_vasylisa@gmail.com>
2024-02-11 16:10:28 +0300

    добавлены параметры из переменных окружения

--- internal/main.go
+++ internal/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/ra4e1/calculator/internal/application"
+import (
+	"os"
+	"strconv"
+
+	"github.com/ra4e1/calculator/internal/application"
+)
 
 func main() { // запуск всего
-	config := application.Config{Port: 8081, CalcDelay: 0}
+	port, err := strconv.Atoi(os.Getenv("CALC_APP_PORT"))
+	if err != nil {
+		port = 8081
+	}
+	calcDelay, err := strconv.Atoi(os.Getenv("CALC_APP_DELAY"))
+	if err != nil {
+		calcDelay = 10
+	}
+
+	config := application.Config{
+		Port:      port,      // Прот http-сервера
+		CalcDelay: calcDelay, // задержка подсчета в секундах
+	}
 	app := application.NewApplication(config)
 	app.Run()
 }
